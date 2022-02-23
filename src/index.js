8c87154d58cd37114e661bc664b8f5a2f100d3b9
+ <+>
2022-02-22 19:03:56 -0700

    commit tree a59395b16bf6a489e0e3c3fd5eee7b811f3f7730

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const init = () => {
+  //select the node
+  const inputForm = document.querySelector('form');
+
+  //add event listener to store input
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('input#searchByID');
   
+
+    //fetch dat from db.json file
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    
+    //converts the json data into an object
+    .then(response => response.json())
+
+    //calls the new object and uses it
+    .then(data => {
+      //accesses the nodes for modification on the DOM
+      const title = document.querySelector('section#movieDetails h4')
+      const summary = document.querySelector('section#movieDetails p')
+      
+      //changes the text of each item on the DOm
+      title.textContent = data.title
+      summary.textContent = data.summary
+
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
